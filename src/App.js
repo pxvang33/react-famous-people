258c335b6fed08077ecdceaa54af037ad0da8087
Peng Xue Vang <pxvang33@gmail.com>
2019-01-15 15:52:15 -0600

    input fields added and able to change DOM

--- src/App.js
+++ src/App.js
@@ -10,6 +10,23 @@ class App extends Component {
                   role: 'Master'
       } // end state
   } // end constructor
+changePerson = (event) =>{
+  console.log(event.target.value);
+  this.setState({
+    famousPerson: event.target.value,
+    // role: event2.target.value
+
+
+  });
+} // end changePerson
+
+changeRole = (event) => {
+  console.log(event.target.value);
+  this.setState({
+    role: event.target.value,
+  });
+} //end changeRole
+
   render() {
     return (
       <div className="App">
@@ -19,11 +36,15 @@ class App extends Component {
             Famous People App
           </p>
 
+          <p>Enter Famous Person</p>
+        <input onChange={this.changePerson} type="text" placeholder="Famous Person"></input>
+        <br/>
+        <input onChange={this.changeRole} type="text" placeholder="Role"></input>
           <p>{this.state.famousPerson} is a famous {this.state.role}.</p>
         </header>
       </div>
     );
-  }
-}
+  } // end render
+} // end App
 
 export default App;
