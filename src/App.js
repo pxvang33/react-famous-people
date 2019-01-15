7fa6eea759e61251443537153125b8d185b1dc31
Peng Xue Vang <pxvang33@gmail.com>
2019-01-15 15:36:55 -0600

    this.state works and shows on DOM

--- src/App.js
+++ src/App.js
@@ -3,22 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+                  famousPerson: 'Jet Li', 
+                  role: 'Master'
+      } // end state
+  } // end constructor
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Famous People App
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+
+          <p>{this.state.famousPerson} is a famous {this.state.role}.</p>
         </header>
       </div>
     );
